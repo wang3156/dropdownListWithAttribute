f9b0a1d0be6acf213ddb9db107a32d72cd4f01b4
Darry Wang <Darry_Wang@jabil.com>
2025-09-16 17:14:37 +0800

    change to React

--- src/components/CSelect.tsx
+++ src/components/CSelect.tsx
@@ -1,85 +1,129 @@
-import { ReactElement, createElement } from "react";
+import React, { ReactElement, createElement, useRef, useState } from "react";
 import { DropdownListWithAttributeContainerProps } from "../../typings/DropdownListWithAttributeProps";
+import { ObjectItem } from "mendix";
 
 let options: DropdownListWithAttributeContainerProps;
-let input_select: HTMLInputElement | null;
+let pre_val_input: string | undefined;
+let showList = false;
 export function CSelect(props: DropdownListWithAttributeContainerProps): ReactElement {
     options = props;
-    console.log('CSelect-------')
+
+
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [searchText, setSearchText] = React.useState("");
+    const [searchResults, setSearchResults] = React.useState<ObjectItem[] | undefined>(options.Options.items);
+
+    React.useEffect(() => {
+        var data = options.Options.items?.filter(it => (options.Op_Label.get(it).value?.toLocaleLowerCase()?.indexOf(searchText.toLocaleLowerCase()) ?? -1) > -1);
+        setSearchResults(data);
+    }, [searchText, options]);
+
+
+    const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout>();
+
+    const renderList = () => {
+        if (!searchResults) {
+            return;
+        }
+        return searchResults.map((item, index) => {
+            return (React.createElement("li", {
+                key: index, id: `content-li${index}`, className: "listItem", onClick: () => {
+                    onSelectLi(item);
+                }, "data-index": index
+            }, options.Op_Label.get(item).value));
+        });
+    };
+
+    const stop_prop = (e: any) => {
+        e.stopPropagation();
+        e.preventDefault()
+    }
+    const onInput_SelectList = () => {
+        stop_prop(event);
+        if (!inputRef.current) {
+            return;
+        }
+        var other_close = () => {
+            if (event?.target == inputRef.current || !showList) return;
+            onInput_SelectList();
+        }
+        let parnt_ipt = inputRef.current.parentElement;
+        let c_cssName = 'r-180';
+
+        if (showList) {
+            parnt_ipt?.classList.remove(c_cssName);
+            document.removeEventListener('click', other_close);
+        } else {
+            parnt_ipt?.classList.add(c_cssName);
+            document.addEventListener('click', other_close, { capture: false })
+        }
+        showList = !showList;
+    }
+    const onInput_Change = () => {
+        let val_input = inputRef.current?.value.trim();
+        if (val_input == pre_val_input) {
+            return;
+        }
+        if (timeoutId) {
+            clearTimeout(timeoutId)
+        }
+        var tid = setTimeout(() => {
+            pre_val_input = val_input;
+            setSearchText(val_input ?? "");
+        }, 300);
+        setTimeoutId(tid);
+    };
+    const onSelectLi = (el: any) => {
+        stop_prop(event);
+        if (!inputRef.current) {
+            return;
+        }
+        options.BindAttr.setTextValue(options.Op_Key.get(el).value?.toString() ?? "")
+        inputRef.current.value = options.Op_Label.get(el).value?.toString() ?? "";
+        onInput_SelectList();
+    }
 
     var el = <div className="css_DropdownListWithAttribute_container">
         <div className="css_DropdownListWithAttribute">
-            <input tabIndex={props.tabIndex} id={props.id} onChange={onInputChange} className="css_input_select" onFocus={onInput_SelectList} readOnly={!props.Can_Search} />
+            <input tabIndex={props.tabIndex} id={props.id} className="css_input_select" onChange={onInput_Change} onFocus={onInput_SelectList} readOnly={!props.Can_Search} ref={inputRef} />
         </div>
-        {create_List(props.Options.items)}
-    </div>
-        ;
+        <ul className="css_DropdownListWithAttribute_list">
+            {renderList()}
+        </ul>
+    </div>;
+
     return el;
 }
 
 
-let pre_val_input: string;
-export function onInputChange() {
-    if (!input_select) {
-        input_select = document.getElementById(options.id) as HTMLInputElement;
-    }
 
-    if (!input_select) {
-        return;
-    }
-    let ipt = input_select;
+// export function onInputChange() {
+//     if (!input_select) {
+//         input_select = document.getElementById(options.id) as HTMLInputElement;
+//     }
 
-    let val_input = ipt.value?.trim();
-    if (!val_input || val_input == pre_val_input) {
-        return;
-    }
-    pre_val_input = val_input;
-    options.BindAttr.setTextValue(val_input);
+//     if (!input_
+// select) {
+//         return;
+//     }
+//     let ipt = input_select;
 
+//     let val_input = ipt.value?.trim();
+//     if (!val_input || val_input == pre_val_input) {
+//         return;
+//     }
+//     pre_val_input = val_input;
 
-}
+//     if (pre_val_input) {
+//         options.Options.items?.filter(pt => {
+//             options.Op_Label.get(pt).value?.startsWith(pre_val_input)
+//         }).map(o => {
 
-let showList = false;
-export function onInput_SelectList() {
+//         })
 
-    if (!input_select) {
-        input_select = document.getElementById(options.id) as HTMLInputElement;
-    }
+//     }
 
-    if (!input_select) {
-        return;
-    }
-    let parnt_ipt = input_select.parentElement;
-    let c_cssName = 'r-180';
-
-    if (showList) {
-        parnt_ipt?.classList.remove(c_cssName);
-
-    } else {
-        parnt_ipt?.classList.add(c_cssName);
-    }
-    showList = !showList;
+// }
 
 
-}
 
-export function create_List(datas: any[] | undefined): ReactElement {
-    let list: any = [];
-    datas?.forEach(element => {
-        list.push(<li onClick={() => {
-            onSelectLi(element)
-        }} key={options.Op_Key.get(element).value?.toString()}>{options.Op_Label.get(element).value}</li>)
-    });
-    return <ul className="css_DropdownListWithAttribute_list">{list}</ul>;
-}
-
-export function onSelectLi(el: any) {
-
-    if (!input_select) {
-        return;
-    }
-    options.BindAttr.setTextValue(options.Op_Key.get(el).value?.toString() ?? "")
-    input_select.value = options.Op_Label.get(el).value?.toString() ?? "";
-    onInput_SelectList();
-
-}
